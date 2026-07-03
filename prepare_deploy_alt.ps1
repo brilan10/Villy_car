3471b691d7cbcd9080fb850ff986afa7376c6256
Yomiy <yomiy@elite-book>
2026-07-03 17:34:17 -0400

    fix: update hostinger db password in deploy script and rebuild webhost_deploy

--- prepare_deploy_alt.ps1
+++ prepare_deploy_alt.ps1
@@ -42,7 +42,7 @@ $content = Get-Content $DbConfigFile -Raw
 $content = $content.Replace("`$host = '127.0.0.1';", "`$host = 'localhost';")
 $content = $content.Replace("`$db   = 'villy_car_db';", "`$db   = 'villycar_base de datos';")
 $content = $content.Replace("`$user = 'root';", "`$user = 'villycar_joel';")
-$content = $content.Replace("`$pass = '';", "`$pass = 'FSKrY2w`$hi-w#TN^';")
+$content = $content.Replace("`$pass = '';", "`$pass = 'abc.123.vilycar';")
 Set-Content -Path $DbConfigFile -Value $content -Encoding Ascii
 
 Write-Host "=========================================="
